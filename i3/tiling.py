a22fba2fbe003777d2b3daf00ab9b3b8e26dee85
George Law <george@geolaw.com>
2022-05-25 12:00:57 -0400

    Update tiling.py
    adding credit

--- i3/tiling.py
+++ i3/tiling.py
@@ -1,4 +1,6 @@
  #!/usr/bin/env python3
+ # credit unknown - I did not write this script from scratch but found it somewhere, reddit, stack over flow, etc ...
+ # will update it if I can find the original author
 import i3ipc
 
 i3          = i3ipc.Connection()
